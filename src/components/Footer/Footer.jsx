fc3321aa1bd19d08648a518e418376f150987ce2
aditya0929 <127277877+aditya0929@users.noreply.github.com>
2023-05-21 08:54:35 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container container">
-          <p className="copyright">DSC-VITB <a href="https://github.com/aditya0929/dsc-task2-website" target="_blank" className="copyright_name">web-dev</a></p>
+          <p className="copyright">created by <a href="https://github.com/aditya0929/dsc-task2-website" target="_blank" className="copyright_name">Aditya</a></p>
           <a href="#home" className="back-top-btn">
             <span className="footer_span">Back To Top</span>
             <BsArrowUp />
