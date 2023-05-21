5d2c947337122287631988092b377aae4d9dcaa1
aditya0929 <127277877+aditya0929@users.noreply.github.com>
2023-05-21 08:53:01 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,21 +15,21 @@ const Contact = () => {
         <p className="contact-desc">come participate in our events and win prizes and be an active member of our community</p>
 
         <div className="contact-mail">
-          <a href="mailto:dsc.vitb@vitbhopal.ac.in" target="_blank" className="contact-link">dsc.vitb@vitbhopal.ac.in</a>
+          <a href="mailto:adityajha8906@gmail.com" target="_blank" className="contact-link">adityajha8906@gmail.com</a>
         </div>
 
         <div className="contact-social">
           
-          <a href="https://www.instagram.com/dscvitbhopal/" target="_blank" className="container-link-bottom">
+          <a href="https://www.instagram.com" target="_blank" className="container-link-bottom">
             <p className="contact_p">Instagram</p><AiOutlineInstagram className="contact-icon" />
           </a>
-          <a href="https://www.linkedin.com/in/data-science-club-vit-bhopal-5b9b02232/?originalSubdomain=in" target="_blank" className="container-link-bottom">
+          <a href="https://www.linkedin.com" target="_blank" className="container-link-bottom">
             <p className="contact_p">Linkedin</p><AiFillLinkedin className="contact-icon" />
           </a>
-          <a href="https://www.youtube.com/channel/UC5pB8eW_kDoimQ5s4S7QgeA" target="_blank" className="container-link-bottom">
+          <a href="https://www.youtube.com" target="_blank" className="container-link-bottom">
             <p className="contact_p">Youtube</p><AiOutlineYoutube className="contact-icon" />
           </a>
-          <a href="https://github.com/cdsvitbhopal" target="_blank" className="container-link-bottom">
+          <a href="https://github.com/aditya0929" target="_blank" className="container-link-bottom">
             <p className="contact_p">Github</p><AiOutlineGithub className="contact-icon" />
           </a>
            
