ad88b46514d16b70014423ddcb0b583b485f5781
aditya0929 <127277877+aditya0929@users.noreply.github.com>
2023-05-21 08:48:51 +0530

    Update Constants.jsx

--- src/components/Constants/Constants.jsx
+++ src/components/Constants/Constants.jsx
@@ -3,32 +3,32 @@ export const eventsData = [
         id: 1,
         image: '/images/event-1.png',
         title: 'DATACAMP - LEARN DATA VISUALIZATION',
-        desc: " Join us for an exciting data science event where you'll learn from leading experts in the field, network with other professionals, and gain hands-on experience with the latest tools and techniques. location- Academic Block(AB-503)",
+        desc: " Join us for an exciting data science event where you'll learn from leading experts in the field, network with other professionals, and gain hands-on experience with the latest tools and techniques.",
         apply: 'https://bvn4lo30u9m.typeform.com/to/VwjXNJcN',
        
     },
     {
         id: 2,
         image: '/images/event-2.png',
-        title: 'DATALIZE-LEARN EDA',
-        desc: "Join us for an exciting data science event where you'll learn from leading experts in the field, network with other professionals, and gain hands-on experience with the latest tools and techniques. location- Academic Block(AB-503)",
+        title: 'UNREAL CREATORS',
+        desc: "Join us for an exciting event where you'll learn from leading experts in the field, network with other professionals, and gain hands-on experience with the latest tools and techniques. ",
         apply: 'https://bvn4lo30u9m.typeform.com/to/VwjXNJcN',
         
     },
     {
         id: 3,
         image: '/images/event-3.png',
-        title: 'Dataverse(upcoming event)',
+        title: 'Metaverse(upcoming event)',
           
-        desc: "Join us for an exciting data science event where you'll learn from leading experts in the field, network with other professionals, and gain hands-on experience with the latest tools and techniques. location- Academic Block(AB-503)",
+        desc: "Join us for an exciting  event where you'll learn from leading experts in the field, network with other professionals, and gain hands-on experience with the latest tools and techniques.",
         apply: 'https://bvn4lo30u9m.typeform.com/to/VwjXNJcN',
         
     },
     {
         id: 4,
         image: '/images/event-4.png',
-        title: 'data-analysis(upcoming event)',
-        desc: "join us for an exciting data science event where you'll learn from leading experts in the field, network with other professionals, and gain hands-on experience with the latest tools and techniques. location- Academic Block(AB-503)",
+        title: 'analysis(upcoming event)',
+        desc: "join us for an exciting event where you'll learn from leading experts in the field, network with other professionals, and gain hands-on experience with the latest tools and techniques.",
         apply: 'https://bvn4lo30u9m.typeform.com/to/VwjXNJcN',
         
     },
@@ -37,23 +37,23 @@ export const eventsData = [
 export const archivesData = [
     {
         id: 1,
-        title: 'Data Science Club Community Projects Arena',
-        desc: 'Welcome to the Data Science Club Community Projects repository! This is a space where members of our community can share their Machine Learning and Data Science projects with others.',
+        title: 'Dummy.ORG Club Community Projects Arena',
+        desc: 'Welcome to the Organization Community Projects repository! This is a space where members of our community can share their projects with others.',
        
-        code: 'https://github.com/cdsvitbhopal/ProjectArena-ML',
+        code: 'https://github.com',
     },
     {
         id: 2,
-        title: 'ML-Sprint-2022',
+        title: 'event-Sprint-2022',
         desc: 'This project showcases work flow of our event.',
        
-        code: 'https://github.com/cdsvitbhopal/ML-Sprint-2022',
+        code: 'https://github.com',
     },
     {
         id: 3,
-        title: 'car price predictor',
-        desc: 'this is a car price predictor',
+        title: 'coding event',
+        desc: 'this is a coding event of javascript',
        
-        code: 'https://github.com/cdsvitbhopal/car-price-prediction',
+        code: 'https://github.com',
     },
 ];
