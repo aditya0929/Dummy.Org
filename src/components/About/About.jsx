13cf593cebe90db48bc5f13d8aea3b233c8957d9
aditya0929 <127277877+aditya0929@users.noreply.github.com>
2023-05-21 08:40:46 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,9 +9,9 @@ const About = () => {
       <div className="about-container container">
           <div className="about-content">
             <h1 className="about-title">More about Us</h1>
-            <p className="about-desc">Our club is open to all students who are interested in data science, regardless of their major or level of experience. We provide opportunities for members to learn about data science concepts and tools, participate in data-driven projects, and connect with other students who share their passion for data.</p>
-            <p className="about-desc">As a member of our club, you can expect to attend workshops and seminars on topics such as data visualization, machine learning, and data ethics. We also offer opportunities for members to work on data science projects and compete in data science competitions.</p>
-            <p className="about-desc about-desc-mb">Joining our data science club is a great way to enhance your skills, connect with peers who share your interests, and make a positive impact in the community. We look forward to welcoming you to our club and helping you achieve your data science goals!</p>
+            <p className="about-desc">Welcome to Dummy.Org, your trusted partner in organizing exceptional educational events. We are a passionate team dedicated to empowering individuals through immersive and transformative learning experiences. With our unwavering commitment to educational excellence, we strive to create a platform where knowledge meets inspiration.</p>
+            <p className="about-desc">As a member of our teams, we understand the importance of fostering a lifelong love for learning. That is why we curate dynamic and engaging events that cater to a wide range of educational interests, from seminars and workshops to conferences and symposiums.</p>
+            <p className="about-desc about-desc-mb">Joining our organsization is a great way to enhance your skills, connect with peers who share your interests, and make a positive impact in the community. We look forward to welcoming you to our club and helping you achieve your data science goals!</p>
             <a href={form} target="_blank" rel="noopener noreferrer">
                 <button className="button about-button">Application form<AiOutlineDownload className="download-icon" /></button>
             </a>
