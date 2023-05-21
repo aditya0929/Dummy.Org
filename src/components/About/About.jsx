fdc3c01ac564a236c79dfe7104e7354d29b99a09
aditya0929 <127277877+aditya0929@users.noreply.github.com>
2023-05-21 08:50:26 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <h1 className="about-title">More about Us</h1>
             <p className="about-desc">Welcome to Dummy.Org, your trusted partner in organizing exceptional educational events. We are a passionate team dedicated to empowering individuals through immersive and transformative learning experiences. With our unwavering commitment to educational excellence, we strive to create a platform where knowledge meets inspiration.</p>
             <p className="about-desc">As a member of our teams, we understand the importance of fostering a lifelong love for learning. That is why we curate dynamic and engaging events that cater to a wide range of educational interests, from seminars and workshops to conferences and symposiums.</p>
-            <p className="about-desc about-desc-mb">Joining our organsization is a great way to enhance your skills, connect with peers who share your interests, and make a positive impact in the community. We look forward to welcoming you to our club and helping you achieve your data science goals!</p>
+            <p className="about-desc about-desc-mb">Joining our organsization is a great way to enhance your skills, connect with peers who share your interests, and make a positive impact in the community. We look forward to welcoming you to our club and helping you achieve your educational goals!</p>
             <a href={form} target="_blank" rel="noopener noreferrer">
                 <button className="button about-button">Application form<AiOutlineDownload className="download-icon" /></button>
             </a>
